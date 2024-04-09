425d5990223500df4392f5e807c3961e86a5bdb2
Francesco Carrara <f.carrara24@studenti.unibg.it>
2024-04-09 09:54:35 +0200

    milestone 1 creation of Evento

---
+++ src/concerti/DateUtilities.java
@@ -0,0 +1,31 @@
+package concerti;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+public class DateUtilities {
+    /**
+     * return true if the date is not passed yet
+     * @param date
+     * @return
+     */
+    public static Boolean isntPassed(LocalDate date){
+        LocalDate now = LocalDate.now();
+        int notPassed = date.compareTo(now);
+        return (notPassed >= 0);
+    }
+
+    /**
+     * retruns date in long format (17 February 2022)
+     * @param date
+     * @return
+     */
+    public static String longFormatDate(LocalDate date){
+
+        DateTimeFormatter df = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.LONG);
+        String formattedDate = date.format(df);
+
+        return formattedDate;
+    }
+}
---
+++ src/concerti/Evento.java
@@ -0,0 +1,86 @@
+package concerti;
+
+import java.time.LocalDate;
+
+public class Evento {
+    private String titolo;
+    private LocalDate data;
+    private int postiTotali;
+    private int postiPrenotati;
+
+    //constructor
+    public Evento(String titolo, LocalDate data, int postiTotali ) throws IllegalArgumentException {
+        if(! DateUtilities.isntPassed(data))
+            throw new IllegalArgumentException("La data dell'evento che stai cercando di inserire è gia passata");
+        else if(postiTotali > 0)
+            throw new IllegalArgumentException("i posti disponibili per l'evento non possono essere negativi");
+        this.titolo = titolo;
+        this.data = data;
+        this.postiTotali = postiTotali;
+        this.postiPrenotati = 0;
+    }
+
+    //getters and setters
+
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public void setData(LocalDate data) throws IllegalArgumentException {
+        if(! DateUtilities.isntPassed(data))
+            throw new IllegalArgumentException("La data dell'evento che stai cercando di inserire è gia passata");
+        this.data = data;
+    }
+
+    //getters
+
+
+    public int getPostiTotali() {
+        return postiTotali;
+    }
+
+    public int getPostiPrenotati() {
+        return postiPrenotati;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+
+    //methods
+    public void prenota(int nPostiDaPrenotare) throws IllegalArgumentException{
+        if(! DateUtilities.isntPassed(data))
+            throw new IllegalArgumentException("L'evento è gia passato");
+        if(nPostiDaPrenotare<0 || nPostiDaPrenotare > (this.postiTotali - this.postiPrenotati)){
+            throw new IllegalArgumentException("non puoi prenotare "+nPostiDaPrenotare+" sono disponibili solo "+(this.postiTotali - this.postiPrenotati));
+        }
+        this.postiPrenotati += nPostiDaPrenotare;
+    }
+
+    public void disdici(int nPostiDaDisdire) throws IllegalArgumentException{
+        if(! DateUtilities.isntPassed(data))
+            throw new IllegalArgumentException("L'evento è gia passato");
+        if(nPostiDaDisdire<0 || nPostiDaDisdire > (this.postiPrenotati)){
+            throw new IllegalArgumentException("non puoi disdire "+nPostiDaDisdire+" sono stati prenotati solo "+this.postiPrenotati);
+        }
+        this.postiPrenotati -= nPostiDaDisdire;
+    }
+
+    // toString
+
+
+    @Override
+    public String toString() {
+        String longDate = DateUtilities.longFormatDate(data);
+        return   longDate +
+                " - " + titolo;
+    }
+}
+
+
